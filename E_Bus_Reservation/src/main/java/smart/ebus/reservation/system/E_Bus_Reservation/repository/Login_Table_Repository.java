98f83298f748629b8f7075d69b5473597b03767c
navin1812ku <navin1812ku@gmail.com>
2023-04-06 19:37:38 +0530

    Creating modules v4

--- E_Bus_Reservation/src/main/java/smart/ebus/reservation/system/E_Bus_Reservation/repository/Login_Table_Repository.java
+++ E_Bus_Reservation/src/main/java/smart/ebus/reservation/system/E_Bus_Reservation/repository/Login_Table_Repository.java
@@ -2,7 +2,6 @@ package smart.ebus.reservation.system.E_Bus_Reservation.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import smart.ebus.reservation.system.E_Bus_Reservation.model.Customer_Details_Table;
 import smart.ebus.reservation.system.E_Bus_Reservation.model.Login_Table;
 
 @Repository
